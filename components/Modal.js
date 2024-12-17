75f7afd93710bf392943b201bab0ac8ba4827640
gimHyoungSoo <gimhyoungsoo@gmail.com>
2024-12-17 17:14:33 +0900

    💄 모달 UI 수정

--- components/Modal.js
+++ components/Modal.js
@@ -1,18 +1,22 @@
-export default function Modal($picture) {
+export default function Modal($picture, $figcap) {
+    const $modal = document.createElement("div");
+    $modal.id = "modal__container";
 
-    const $Modal = document.createElement("div")
-    $Modal.classList.add("modal__top--container")
+    const $wrapper = document.createElement("div");
+    $wrapper.classList.add("modal__top--wrapper");
 
-    const $pictureWrapper = document.createElement("div")
-    $pictureWrapper.classList.add("modal__picture--wrapper")
+    const $pictureWrapper = document.createElement("div");
+    $pictureWrapper.classList.add("modal__picture--wrapper");
 
-    $pictureWrapper.appendChild($picture)
+    $figcap.classList.add("modal__figcap--wrapper")
 
-    $Modal.appendChild($pictureWrapper)
+    $pictureWrapper.appendChild($picture);
+    $wrapper.append($figcap, $pictureWrapper );
+    $modal.appendChild($wrapper);
 
-    $Modal.onclick = () => {
-        document.body.removeChild($Modal);
-    }
+    $modal.onclick = () => {
+        document.body.removeChild($modal);
+    };
 
-    return $Modal
+    return $modal;
 }
