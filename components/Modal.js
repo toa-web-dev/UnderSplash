8be2c9f3aaa9bf21a472a6ea7dfba3a1989e0b85
gimHyoungSoo <gimhyoungsoo@gmail.com>
2024-12-18 22:44:55 +0900

    💄 Modal 컴포넌트 스타일 수정

--- components/Modal.js
+++ components/Modal.js
@@ -1,17 +1,18 @@
 export default function Modal($picture, $figcap) {
     const $modal = document.createElement("div");
     $modal.id = "modal";
+    $modal.classList.add("center");
 
     const $wrapper = document.createElement("div");
     $wrapper.classList.add("modal__content");
 
     const $pictureWrapper = document.createElement("div");
-    $pictureWrapper.classList.add("modal__picture-wrapper");
+    $pictureWrapper.classList.add("modal__picture-wrapper", "center");
 
-    $figcap.classList.add("modal__figcap")
+    $figcap.classList.add("modal__figcap", "center");
 
     $pictureWrapper.appendChild($picture);
-    $wrapper.append($figcap, $pictureWrapper );
+    $wrapper.append($figcap, $pictureWrapper);
     $modal.appendChild($wrapper);
 
     $modal.onclick = () => {
