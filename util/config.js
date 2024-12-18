b318772169a8a7bf3593a6456938c2f6726619c0
gimHyoungSoo <gimhyoungsoo@gmail.com>
2024-12-18 16:37:16 +0900

    ✨intersectionObserver로 뷰포트가 마지막 사진에 도달하면 다음 이미지를 요청하여 렌더링 💩 - 이미지가 쌓일 수록 각 grid--column의 길이 차이가 커짐 - 이미지 로딩 로직 개선 필요

---
+++ util/config.js
@@ -0,0 +1,8 @@
+const URL = "https://picsum.photos/id/";
+const DEVICE_WIDTH = {
+    laptop: 1440,
+    tablet: 768,
+    mobile: 425,
+};
+
+export { URL, DEVICE_WIDTH};
--- util/getAPI.js
+++ util/getAPI.js
@@ -1,21 +1,54 @@
-const URL = "https://picsum.photos/id/";
-const DEVICE_WIDTH = {
-    laptop: 1440,
-    tablet: 768,
-    mobile: 425,
-};
+import { URL, DEVICE_WIDTH } from "../util/config.js";
+function createPagination() {
+    let nextPageUrl = null; // 상태를 클로저 내에 은닉
 
+    return {
+        setNextPageUrl(url) {
+            nextPageUrl = url;
+        },
+        getNextPageUrl() {
+            return nextPageUrl;
+        },
+    };
+}
+const pagination = createPagination();
 export async function getAPI(params) {
     try {
-        const response = await fetch("https://picsum.photos/v2/list?page=6&limit=30", { cache: "force-cache" });
+        // 요청 시작
+
+        let response = null;
+        if (pagination.getNextPageUrl() !== null) {
+            response = await fetch(pagination.getNextPageUrl(), { cache: "force-cache" });
+        } else {
+            response = await fetch("https://picsum.photos/v2/list?page=6&limit=10", { cache: "force-cache" });
+        }
 
         if (!response.ok) {
             // HTTP 에러코드도
             throw new Error("Network response was not ok");
         }
 
-        const result = await response.json();
+        // 응답 성공 후
+        const linkHeader = response.headers.get("Link");
+        if (linkHeader) {
+            // Link 헤더 파싱
+            const links = linkHeader
+                .split(",") // 각 링크 분리
+                .map((part) => part.match(/<([^>]+)>;\s*rel="([^"]+)"/)) // 정규식 매칭
+                .reduce((acc, match) => {
+                    if (match) {
+                        const [, url, rel] = match; // url과 rel 값을 추출
+                        acc[rel] = url;
+                    }
+                    return acc;
+                }, new Object());
 
+            pagination.setNextPageUrl(links.next);
+        } else {
+            console.log("Link 헤더가 없습니다.");
+        }
+
+        const result = await response.json();
         const data = result.map((img) => {
             for (const [device, deviceWidth] of Object.entries(DEVICE_WIDTH)) {
                 const deviceHeight = Math.floor((deviceWidth * img.height) / img.width);
