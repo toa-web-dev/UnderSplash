d8e2a1c1ce5aac225ac0a1eb0383ca9cc1aea2f5
gimHyoungSoo <gimhyoungsoo@gmail.com>
2024-12-21 17:40:07 +0900

    💩 반응형을 위한 grid column 개수를 화면너비에 따라 조절하는 로직 개발 중 - 현재 config의 장치 뷰포트 너비에 따라 column 개수가 조절되는 코드 완성 - 조절된 column에 이미지를 append하는 로직 필요

--- app.js
+++ app.js
@@ -2,29 +2,58 @@ import FeedItem from "./components/FeedItem.js";
 import { getAPI } from "./util/getAPI.js";
 import initIO from "./util/intersectionIO.js";
 
+let columnNumber;
 window.addEventListener("DOMContentLoaded", () => {
-    initFeed();
+    // initFeed();
+
+    if (window.innerWidth <= 425) {
+        columnNumber = 1;
+    } else if (window.innerWidth <= 768) {
+        columnNumber = 2;
+    } else {
+        columnNumber = 3;
+    }
 });
 
-async function initFeed(params) {
-    appendFeedItem().then(() => {
-        const $IO = document.createElement("div");
-        $IO.classList.add("io");
-        document.querySelector("#main").appendChild($IO);
+window.addEventListener("resize", () => {
+    let newColumnNumber;
+    if (window.innerWidth <= 425) {
+        newColumnNumber = 1;
+    } else if (window.innerWidth <= 768) {
+        newColumnNumber = 2;
+    } else {
+        newColumnNumber = 3;
+    }
 
-        const IO = initIO(appendFeedItem);
-        IO.observe($IO);
-    });
-}
-async function appendFeedItem() {
-    const data = await getAPI();
-    const $gridColumns = document.querySelectorAll(".main__grid-column");
-    let columnIndex = 0;
-    data.forEach((element) => {
-        $gridColumns[columnIndex].appendChild(FeedItem(element));
-        columnIndex++;
-        if (columnIndex > 2) {
-            columnIndex = 0;
-        }
-    });
-}
+    // columnNumber가 변할때 컬럼의 수를 변경하고, FeedItem을 다시 append한다.
+    if (columnNumber === newColumnNumber) return;
+    columnNumber = newColumnNumber;
+
+    const $grid = document.querySelector(".main__grid-container");
+    $grid.replaceChildren(""); // $grid의 내부를 비운다
+
+    //columnNumber 만큼 grid의 열을 추가한다.
+    for (let i = 0; i < columnNumber; i++) {
+        const $column = document.createElement("div");
+        $column.classList.add("main__grid-column");
+        $grid.appendChild($column);
+    }
+});
+// function initFeed(params) {
+
+// }
+// async function name(params) {
+//     const data = await getAPI();
+
+//     //Figure 컴포넌트가 요소인 배열을 만든다
+//     const feedItemArray = [];
+//     data.forEach((data) => {
+//         feedItemArray.push(FeedItem(data));
+//     });
+
+//     const $gridColumns = document.querySelectorAll(".main__grid-column");
+
+//     data.forEach((element, idx) => {
+//         $gridColumns[idx % $gridColumns.length].appendChild(FeedItem(element));
+//     });
+// }
